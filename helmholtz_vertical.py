51b1e44a7224ff2daeac349777c278694ee4c1f8
Colin Cotter <colin.cotter@imperial.ac.uk>
2023-12-07 18:32:42 +0000

    stashing sketch

--- helmholtz_vertical.py
+++ helmholtz_vertical.py
@@ -3,17 +3,18 @@ from firedrake import *
 #using hybridisation with LU
 
 n = 50
-mesh = UnitSquareMesh(n,n)
+L = 1.0
+m = IntervalMesh(n, L)
+mesh = ExtrudedMesh(m, layers=n)
 
 k = Constant(1.0)
 ee = Constant(1.0)
-ee0 = Constant(1.0)
 
 deg = 1
-BDMBroken = BrokenElement(FiniteElement("BDM", "triangle", deg))
-V = VectorFunctionSpace(mesh, BDMBroken)
+RTBroken = BrokenElement(FiniteElement("RTCF", "quadrilateral", deg))
+V = VectorFunctionSpace(mesh, RTBroken)
 Q = VectorFunctionSpace(mesh, "DG", deg-1)
-Tr = VectorFunctionSpace(mesh, "HDiv Trace", deg)
+Tr = VectorFunctionSpace(mesh, "HDiv Trace", deg-1)
 W = MixedFunctionSpace((V,Q,Tr))
 
 u, p, ll = TrialFunctions(W)
@@ -32,8 +33,6 @@ qi = q[1]
 mur = mu[0]
 mui = mu[1]
 
-r0 = Constant(1.0)
-
 n = FacetNormal(mesh)
 
 llrS = llr('+')
@@ -41,28 +40,31 @@ lliS = lli('+')
 murS = mur('+')
 muiS = mui('+')
 
+dS = dS_h + dS_v
+ds = ds_tb + ds_v
+
 a = (
     #ur equation
-    (ee*inner(vr,ur) - k*inner(vr, ui) - r0*inner(div(vr), pr))*dx
-    + r0*jump(vr,n)*llrS*dS + r0*inner(vr,n)*llr*ds
+    (ee*inner(vr,ur) - k*inner(vr, ui) - inner(div(vr), pr))*dx
+    + jump(vr,n)*llrS*dS + inner(vr,n)*llr*ds
     #pr equation
-    + (r0*inner(qr, div(ur)) - k*qr*pi + ee*qr*pr)*dx
+    + (inner(qr, div(ur)) - k*qr*pi + ee*qr*pr)*dx
     #mur equation
-    + r0*jump(ur,n)*murS*dS + r0*inner(ur,n)*mur*ds
+    + jump(ur,n)*murS*dS + inner(ur,n)*mur*ds
     #ui equation
-    + (ee*inner(vi,ui) + k*inner(vi, ur) - r0*inner(div(vi), pi))*dx
-    + r0*jump(vi,n)*lliS*dS + r0*inner(vi,n)*lli*ds
+    + (ee*inner(vi,ui) + k*inner(vi, ur) - inner(div(vi), pi))*dx
+    + jump(vi,n)*lliS*dS + inner(vi,n)*lli*ds
     #pi equation
-    + (r0*inner(qi, div(ui)) + k*qi*pr + ee*qi*pi)*dx
+    + (inner(qi, div(ui)) + k*qi*pr + ee*qi*pi)*dx
     #mui equation
-    + r0*jump(ui,n)*muiS*dS + r0*inner(ui,n)*mui*ds
+    + jump(ui,n)*muiS*dS + inner(ui,n)*mui*ds
     )
 
 x, y = SpatialCoordinate(mesh)
 
 f = exp(-((x-0.5)**2 + (y-0.5)**2)/0.25**2)
 
-L = qi*f/k*dx
+L = qi*f*dx
 
 U = Function(W)
 
@@ -75,26 +77,24 @@ condensed_params = {'ksp_type':'preonly',
 
 hybrid_params = {
 'mat_type':'matfree',
-'ksp_type':'gmres',
-    'ksp_converged_reason': None,
+'ksp_type':'preonly',
+'ksp_converged_reason': None,
 'pc_type':'python',
 'pc_python_type':'firedrake.SCPC',
 'pc_sc_eliminate_fields':'0,1',
 'condensed_field':condensed_params}
 
-# 'ksp_type':'gmres',
-# 'mat_type':'aij',
-# 'ksp_monitor': None ,
-# 'pc_type':'python',
-# 'ksp_rtol': 1E -8 ,
-# 'pc_python_type':'firedrake.GTMGPC',
-# 'gt': {'mat_type':'aij',
-# 'mg_levels':{'ksp_type':'chebyshev',
-# 'ksp_max_it': 2 ,
-# 'pc_type':'bjacobi',
-# 'sub_pc_type':'sor'} ,
-# 'mg_coarse': mg_param }}}
-# appctx = {’ get_coarse_operator ’ : p1_callback ,
+condensed_gt_params = {'ksp_type':'fgmres',
+                       'ksp_monitor': None,
+                       'pc_type':'python',
+                       'pc_python_type':'firedrake.GTMGPC',
+                       'gt': {'mat_type':'aij',
+                              'mg_levels':{'ksp_type':'gmres',
+                                           'ksp_max_it': 3,
+                                           'pc_type':'bjacobi',
+                                           'sub_pc_type':'SHOULD BE ASM'} ,
+                              'mg_coarse': mg_param }}}
+appctx = {’ get_coarse_operator ’ : p1_callback ,
 # 22 ’ get_coarse_space ’ : get_p1_space ,
 # 23 ’ interpolation_matrix ’: interpolation_matrix }
 # mg_param = {’ksp_type ’: ’preonly ’,
@@ -116,8 +116,7 @@ file0 = File('hh.pvd')
 u_out = Function(V)
 p_out = Function(Q)
 
-for e0 in (1.0, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001):
-    ee.assign(e0)
-    r0.assign(1.0)
+for k0 in range(20):
+    k.assign(k0)
     U.assign(0.)
     HSolver.solve()
